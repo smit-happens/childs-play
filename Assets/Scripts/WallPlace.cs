7139a4858ba000b34cc04a33ae1b147301e3873d
Alexander Smith <asmit147@ycp.edu>
2017-04-01 04:42:32 -0400

    upload of holotoolkit

---
+++ Assets/Scripts/WallPlace.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class WallPlace : MonoBehaviour {
+
+    GestureRecognizer recognizer = new GestureRecognizer();
+
+
+    // Use this for initialization
+    void Start () {
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        //recognizer.TappedEvent += TapEventHandler;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if ()
+        //{
+
+        //}
+        //RaycastHit Hit;
+        //Ray 
+
+        //transform.rotation = Quaternion.LookRotation(-raycastHit.normal);
+
+        
+    }
+
+    void OnDestroy()
+    {
+        //recognizer.TappedEvent -= TapEventHandler;
+    }
+
+    public void TapEventHandler()
+    {
+
+    }
+
+}
