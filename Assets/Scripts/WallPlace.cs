f203a51979fecb82f1e342538506e95135092f83
Alexander Smith <asmit147@ycp.edu>
2017-04-01 06:39:22 -0400

    detects whether you're infront of a hologram or not

--- Assets/Scripts/WallPlace.cs
+++ Assets/Scripts/WallPlace.cs
@@ -5,38 +5,55 @@ using UnityEngine.VR.WSA.Input;
 
 public class WallPlace : MonoBehaviour {
 
-    GestureRecognizer recognizer = new GestureRecognizer();
-
+    GestureRecognizer recognizer;
+    private MeshRenderer currentMesh;
 
     // Use this for initialization
     void Start () {
+        //setup the gesture recognizer
+        recognizer = new GestureRecognizer();
         recognizer.SetRecognizableGestures(GestureSettings.Tap);
-        //recognizer.TappedEvent += TapEventHandler;
+        recognizer.TappedEvent += TapEventHandler;
+        recognizer.StartCapturingGestures();
+
+        //save the mesh renderer that's on the same object as this script
+        currentMesh = this.gameObject.GetComponentInChildren<MeshRenderer>();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //if ()
-        //{
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
 
-        //}
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            Debug.Log("True");
+        }
+        else
+        {
+            Debug.Log("False");
+        }
         //RaycastHit Hit;
         //Ray 
 
         //transform.rotation = Quaternion.LookRotation(-raycastHit.normal);
 
-        
+
     }
 
     void OnDestroy()
     {
-        //recognizer.TappedEvent -= TapEventHandler;
+        recognizer.TappedEvent -= TapEventHandler;
     }
 
-    public void TapEventHandler()
+    void TapEventHandler(InteractionSourceKind source, int tapNum, Ray headRay)
     {
+        Debug.Log("Pinch received!");
 
+        
     }
 
 }
